6241982f014caa15f75d998ab901028f34329f92
Zain Muhammad <58492711+hundooo@users.noreply.github.com>
2022-10-11 16:06:35 -0400

    create script to initialize database

---
+++ init_db.py
@@ -0,0 +1,10 @@
+from multiprocessing import connection
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+connection.commit()
+connection.close()
